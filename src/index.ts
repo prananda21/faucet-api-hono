f9588c1bbd8da95def5cd9fed50a5da2d2385ac7
Prananda Yoga <pranandayoga21@gmail.com>
2025-04-03 21:29:54 +0800

    feat: add global error handler and develop on transaction handler

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ import { describeRoute, openAPISpecs } from 'hono-openapi';
 import { resolver } from 'hono-openapi/zod';
 import { transactionResponseSchema } from './utils/validation/schema';
 import { swaggerUI } from '@hono/swagger-ui';
+import { TransactionRepository } from './database/repository/implementation';
+import { isHttpError } from './utils/function';
+import { ContentfulStatusCode } from 'hono/utils/http-status';
+import { BadRequestException } from './utils/error/custom';
 
 const app = new Hono();
+const transactionRepo = new TransactionRepository();
 
 /**
  * Routes
@@ -54,7 +59,22 @@ app.post(
   }),
   transactionValidatorMiddleware,
   async (c) => {
-    const validate = c.req.valid('json');
+    const { walletAddress } = c.req.valid('json');
+
+    /**
+     * Check database first
+     */
+    const alreadyTransaction = await transactionRepo.hasWalletDripToday(
+      walletAddress,
+    );
+    if (alreadyTransaction) {
+      throw new BadRequestException(
+        'Limitation Error',
+        'Only one transaction for one wallet per day, try in 24 hours',
+      );
+    }
+
+    return c.json({ message: 'hello' });
   },
 );
 
@@ -81,6 +101,23 @@ app.get(
   }),
 );
 app.get('/docs', swaggerUI({ url: 'openapi' }));
+
+/**
+ * Global Error Handler
+ */
+app.onError((err, c) => {
+  console.error('Unhandled error: ', err);
+  const status = isHttpError(err) ? err.status : 500;
+
+  return c.json(
+    {
+      data: null,
+      errors: err.name,
+      message: err.message || 'Internal Server Error',
+    },
+    status as ContentfulStatusCode,
+  );
+});
 
 export default {
   port: 1000,
