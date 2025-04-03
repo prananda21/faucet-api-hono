7bd3d709a32cad6aa2b7ffbf81dcdf75ec0142a9
Prananda Yoga <pranandayoga21@gmail.com>
2025-04-03 20:12:46 +0800

    docs: add swagger UI docs and schema docs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,88 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { transactionValidatorMiddleware } from './middleware/validation';
+import { describeRoute, openAPISpecs } from 'hono-openapi';
+import { resolver } from 'hono-openapi/zod';
+import { transactionResponseSchema } from './utils/validation/schema';
+import { swaggerUI } from '@hono/swagger-ui';
 
-const app = new Hono()
+const app = new Hono();
 
+/**
+ * Routes
+ */
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json({ message: `Welcome to Faucet API!` });
+});
+
+/**
+ * Transaction routes
+ */
+app.post(
+  '/drip',
+  describeRoute({
+    description: 'Do Transaction for Token Request.',
+    tags: ['Transactions'], // Add one or more tags here
+    requestBody: {
+      description: 'Payload for token request',
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              walletAddress: {
+                type: 'string',
+                description: 'Unique Address of target wallet.',
+                example: '0x...', // Example value for amount
+              },
+            },
+            required: ['walletAddress'],
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'Successful sending token.',
+        content: {
+          'application/json': {
+            schema: resolver(transactionResponseSchema),
+          },
+        },
+      },
+    },
+  }),
+  transactionValidatorMiddleware,
+  async (c) => {
+    const validate = c.req.valid('json');
+  },
+);
+
+/**
+ * Documentation routes
+ */
+app.get(
+  '/openapi',
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: 'Faucet API',
+        version: '1.0.0',
+        description:
+          'API for Faucet Transaction or Distribution Token for short.',
+      },
+      servers: [
+        {
+          url: 'http://localhost:1000',
+          description: 'Local server',
+        },
+      ],
+    },
+  }),
+);
+app.get('/docs', swaggerUI({ url: 'openapi' }));
 
 export default {
   port: 1000,
-  fetch: app.fetch
-}
+  fetch: app.fetch,
+};
--- src/utils/validation/schema.ts
+++ src/utils/validation/schema.ts
@@ -1,5 +1,29 @@
-import {z} from 'zod'
+import { z } from 'zod';
+import 'zod-openapi/extend';
 
 export const transactionSchema = z.object({
-  walletAddress: z.string().min(42).regex(/^0x[a-fA-F0-9]{40}$/)
-})
+  walletAddress: z
+    .string()
+    .min(42)
+    .regex(/^0x[a-fA-F0-9]{40}$/)
+    .openapi({ example: '0x...' }),
+});
+
+/**
+ * Response Schema
+ */
+export const transactionResponseSchema = z
+  .object({
+    transactionHash: z.string(),
+    walletAddress: z.string(),
+    tokenValue: z.string(),
+    status: z.enum(['PENDING', 'SUCCESS', 'FAILED']),
+  })
+  .openapi({
+    example: {
+      walletAddress: '0x...',
+      tokenValue: '100 ETH',
+      transactionHash: '0x...',
+      status: 'PENDING',
+    },
+  });
